925f8460f9f183491855f92904541512a53ede35
gautam3000 <58000693+gautam3000@users.noreply.github.com>
2020-04-20 15:59:46 +0530

    Update WildCardAverageFunction.java

--- WildCardAverageFunction.java
+++ WildCardAverageFunction.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 class Data < T extends Number>
 {
@@ -18,20 +18,18 @@ class Data < T extends Number>
     }
 
     void sameAverage(Data<?> generics) {
-        double a = Average() ;
         if(Average() == generics.Average()) {
-            System.out.println("Average is same ");
+            System.out.println("Average is same.");
         }
         else{
             System.out.println("Average is different");
         }
     }
 } 
-
-public class GenericsSameAverage{
+public class GC{
     public static void main(String[] args) {
-        Generics<Integer> n1 = new Generics<Integer> (new Integer[] { 10,20,30,40,50} );        ,
-        Generics<Integer> n2 = new Generics<Integer> (new Integer[] { 10,20,30,40,50] } );        
+        Generics<Integer> n1 = new Generics<Integer> (new Integer[] { 10,20,30,40,50} );        
+        Generics<Integer> n2 = new Generics<Integer> (new Integer[] { 10,20,30,40,50 } );        
         Generics<Double> n3 = new Generics<Double> (new Double[] { 10.3,20.5,30.6,40.4,50.9 } );        
         Generics<Float> n4 = new Generics<Float> (new Float[] {10.3f,20.5f,30.6f,40.4f,50.9f } );   
         n1.sameAverage(n2);
