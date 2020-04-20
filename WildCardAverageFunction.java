357597e36a4530814172d113ba3d20a0c54a82cd
gautam3000 <58000693+gautam3000@users.noreply.github.com>
2020-04-20 15:57:15 +0530

    Update WildCardAverageFunction.java

--- WildCardAverageFunction.java
+++ WildCardAverageFunction.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 
-class Generics < TypeCasting extends Number>
+class Data < T extends Number>
 {
-    TypeCasting Genericarray[];
-    Generics (TypeCasting arr[]) {
-        Genericarray = arr;
+    T array[];
+    Data (T arr[]) {
+        array = arr;
     }
 
     double Average() {
         double TotalAverage = 0, sum = 0 ;
-        int abcd = Genericarray.length ;
-        for (int i = 0 ; i < abcd ; i++)
+        for (int i = 0 ; i < array.length ; i++)
         {
-            sum += Genericarray[i].doubleValue();
+            sum += array[i].doubleValue();
         }
-        TotalAverage = sum / abcd ;
-        // System.out.println("The average is " + TotalAverage);
+        TotalAverage = sum / array.length ;
         return TotalAverage;
     }
 
-    void sameAverage(Generics<?> generics) {
+    void sameAverage(Data<?> generics) {
         double a = Average() ;
         if(Average() == generics.Average()) {
-            System.out.println("Average is same " + a);
+            System.out.println("Average is same ");
         }
         else{
             System.out.println("Average is different");
@@ -32,51 +30,11 @@ class Generics < TypeCasting extends Number>
 
 public class GenericsSameAverage{
     public static void main(String[] args) {
-
-        int numberz = 5 ;
-        Scanner sc = new Scanner(System.in);
-        int    [] intArray1      = new int    [numberz];
-        int    [] intArray2      = new int    [numberz];
-        double [] doubleArray    = new double [numberz];
-        float  [] floatArray     = new float  [numberz];
-        
-        System.out.println("Taking input for intArray1 ");
-        System.out.println("Please enter 5 integer:- ");
-        for(int i = 0 ; i < numberz ; i++)
-        {
-            intArray1[i] = sc.nextInt();
-        }
-
-        System.out.println("Taking input for intArray2 ");
-        System.out.println("Please enter 5 integer:- ");
-        for(int i = 0 ; i < numberz ; i++)
-        {
-            intArray2[i] = sc.nextInt();
-        }
-
-        System.out.println("Taking input for doubleArray:- ");
-        System.out.println("Please enter 5 double number:- ");
-        for(int i = 0 ; i < numberz ; i++)
-        {
-            doubleArray[i] = sc.nextDouble();
-        }
-
-        System.out.println("Taking input for floatArray:- ");
-        System.out.println("Please enter 5 float number:- ");
-        for(int i = 0 ; i < numberz ; i++)
-        {
-            floatArray[i] = sc.nextFloat();
-        }
-
-        sc.close();
-        Generics<Integer> num1 = new Generics<Integer> (new Integer[] {intArray1[0], intArray1[1], intArray1[2], intArray1[3], intArray1[4] } );        
-        Generics<Integer> num2 = new Generics<Integer> (new Integer[] {intArray2[0], intArray2[1], intArray2[2], intArray2[3], intArray2[4] } );        
-        Generics<Double> decimalnum1 = new Generics<Double> (new Double[] {doubleArray[0], doubleArray[1], doubleArray[2], doubleArray[3], doubleArray[4] } );        
-        Generics<Float> decimalnum2 = new Generics<Float> (new Float[] {floatArray[0], floatArray[1], floatArray[2], floatArray[3], floatArray[4] } );  
-
-        System.out.print("For intArray1 and intArray2  ");
-        num1.sameAverage(num2);
-        System.out.print("For doubleArray and floatArray ");
-        decimalnum1.sameAverage(decimalnum2);
+        Generics<Integer> n1 = new Generics<Integer> (new Integer[] { 10,20,30,40,50} );        ,
+        Generics<Integer> n2 = new Generics<Integer> (new Integer[] { 10,20,30,40,50] } );        
+        Generics<Double> n3 = new Generics<Double> (new Double[] { 10.3,20.5,30.6,40.4,50.9 } );        
+        Generics<Float> n4 = new Generics<Float> (new Float[] {10.3f,20.5f,30.6f,40.4f,50.9f } );   
+        n1.sameAverage(n2);
+        n3.sameAverage(n4);
     }
 }
